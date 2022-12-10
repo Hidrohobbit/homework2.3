321186a863f5fcf05946a407149af6c467636fb9
Stas Mishin <hidrohobbit@yandex.ru>
2022-12-10 20:07:14 +0400

    correction of mistakes

--- homework_2.3.1/homework_2.3.1.cpp
+++ homework_2.3.1/homework_2.3.1.cpp
@@ -9,20 +9,21 @@ private:
     double num1;
     double num2;
 
+public:
 
-    double add (double num1, double num2) { return num1 + num2; }
+    double add()          { return num1 + num2; }
 
-    double multiply (double num1, double num2) { return num1 * num2; }
+    double multiply()     { return num1 * num2; }
 
-    double subtract_1_2 (double num1, double num2) { return num1 - num2; }
+    double subtract_1_2() { return num1 - num2; }
 
-    double subtract_2_1 (double num1, double num2) { return num2 - num1; }
+    double subtract_2_1() { return num2 - num1; }
 
-    double divide_1_2 (double num1, double num2) { return num1 / num2; }
+    double divide_1_2()   { return num1 / num2; }
 
-    double divide_2_1 (double num1, double num2) { return num2 / num1; }
+    double divide_2_1()   { return num2 / num1; }
 
-    bool set_num1 (double num1)
+    bool   set_num1(double num1)
     {
         if (num1 != 0)
         {
@@ -30,10 +31,14 @@ private:
             return true;
         }
         else
+        {
+            std::cout << " Значение не может быть равно 0.\nУстановлено значение по умолчанию (1)" << std::endl;
             return false;
+        }
+            
     }
 
-    bool set_num2 (double num2)
+    bool   set_num2(double num2)
     {
         if (num2 != 0)
         {
@@ -41,27 +46,22 @@ private:
             return true;
         }
         else
+        {
+            std::cout << " Значение не может быть равно 0.\nУстановлено значение по умолчанию (1)" << std::endl;
             return false;
+        }
+            
     }
 
-    
-
-public:
-
-    void get_result(double num1,double num2)
+    void   get_result()
     {
-        if (set_num1(num1) == false || set_num2(num2) == false)
-        {
-            std::cout << "\nНеверный ввод!!!" << std::endl;
-        }
-        else
-        {
-            double result_add          = add(num1, num2);
-            double result_multiply     = multiply(num1, num2);
-            double result_subtract_1_2 = subtract_1_2(num1, num2);
-            double result_subtract_2_1 = subtract_2_1(num1, num2);
-            double result_divide_1_2   = divide_1_2(num1, num2);
-            double result_divide_2_1   = divide_2_1(num1, num2);
+       
+            double result_add          = add();
+            double result_multiply     = multiply();
+            double result_subtract_1_2 = subtract_1_2();
+            double result_subtract_2_1 = subtract_2_1();
+            double result_divide_1_2   = divide_1_2();
+            double result_divide_2_1   = divide_2_1();
 
             std::cout << "\nnum1 + num2 = " << result_add
                       << "\nnum1 * num2 = " << result_multiply
@@ -70,7 +70,13 @@ public:
                       << "\nnum1 / num2 = " << result_divide_1_2
                       << "\nnum2 / num1 = " << result_divide_2_1
                       << std::endl;
-        }
+     
+    }
+
+    Calculator()
+    {
+        num1 = 1;
+        num2 = 1;
     }
 };
 
@@ -88,17 +94,33 @@ int main()
 
     while(true)
     {
+        std::cout << std::endl;
         std::cout << "Введите num1: ";
         std::cin >> num1;
+        calc.set_num1(num1);
 
         std::cout << "\nВведите num2: ";
         std::cin >> num2;
+        calc.set_num2(num2);
 
-        calc.get_result(num1, num2);
+        
+
+        std::cout << "\nnum1 + num2 = " << calc.add()
+            << "\nnum1 * num2 = " << calc.multiply()
+            << "\nnum1 - num2 = " << calc.subtract_1_2()
+            << "\nnum2 - num1 = " << calc.subtract_2_1()
+            << "\nnum1 / num2 = " << calc.divide_1_2()
+            << "\nnum2 / num1 = " << calc.divide_2_1()
+            << std::endl;
 
         std::cout << std::endl;
+
+        std::cout << "Вывод методом get_result " << std::endl;
+
+        calc.get_result();
+
+
     }
 
-    return 0;
-    
+    return 0;   
 }
--- homework_2.3.2/homework_2.3.2.cpp
+++ homework_2.3.2/homework_2.3.2.cpp
@@ -5,109 +5,108 @@
 class Counter
 {
 private:
-    
+
     int number;
-
-    int  add()       {return ++number;}
-    int  subtract()  {return --number;}
-    void show()
-    {
-        std::cout << number;
-        std::cout << std::endl;
-    }
-
-
-
-
-    void start_count(std::string select)
-    {
-        if (select == "+")
-        {
-            add();
-        }
-        else if (select == "-")
-        {
-            subtract();
-        }
-        else if (select == "=")
-        {
-            show();
-        }
-        else
-        {
-            std::cout << std::endl;
-            std::cout << "Неизвестная команда";
-        }
-
-
-       
-
-
-    }
-
 
 public:
 
-    void counter_menu()
-    {
-        std::string set_operation;
-        std::string set_initial_value;
+    void  add()                 { ++number; }
+    void  subtract()            { --number; }
+    void  set_value(int number) { this->number = number; }
+    int   get_value()           { return number; }
 
-        std::cout << "Вы хотите указать начальное значение счётчика? Введите y или n: ";
-        std::cin >> set_initial_value;
-        
-        std::cout << std::endl;
-
-        if (set_initial_value == "y")
-        {
-            std::cout << "Введите начальное значение счётчика: ";
-            std::cin >> number;
-        }
-        else if (set_initial_value == "n")
-        {
-            std::cout << "Начальное значение по умолчанию: " << number;
-            std::cout << std::endl;
-        }
-
-        
-
-        while(true)
-        {
-            std::cout << std::endl;
-            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-            std::cin >> set_operation;
-
-            if (set_operation == "x")
-            {
-                std::cout << std::endl;
-                std::cout << "Завершение работы программы!!!";
-                std::cout << std::endl;
-                break;
-            }
-            else
-            start_count(set_operation);
-
-        }
-        
-
-    }
-    
     Counter()
     {
         number = 1;
     }
-   
+
 };
 
+void start_count(Counter& count, std::string select)
+{
+    int result = 0;
 
+    if (select == "+")
+    {
+        count.add();
+    }
+    else if (select == "-")
+    {
+        count.subtract();
+    }
+    else if (select == "=")
+    {
+        std::cout << count.get_value();
+    }
+    else
+    {
+        std::cout << std::endl;
+        std::cout << "Неизвестная команда";
+    }
+}
+
+void counter_menu()
+{
+    int number = 1;
+    std::string set_operation;
+    std::string set_initial_value;
+
+    Counter count;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите y или n: ";
+    std::cin >> set_initial_value;
+    std::cout << std::endl;
+
+    if (set_initial_value == "y")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> number;
+        count.set_value(number);
+    }
+    else if (set_initial_value == "n")
+    {
+        std::cout << "Начальное значение по умолчанию: " << number;
+   
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << "Неизвестная команда!!!" << std::endl;
+        counter_menu();
+    }
+
+
+
+    while (true)
+    {
+        std::cout << std::endl;
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> set_operation;
+
+        if (set_operation == "x")
+        {
+            std::cout << std::endl;
+            std::cout << "Завершение работы программы!!!";
+            std::cout << std::endl;
+            break;
+        }
+        else if ((set_operation == "+") || (set_operation == "-") || (set_operation == "="))
+        {
+            start_count(count, set_operation);
+        }
+        else
+        {
+            std::cout << "Неизвестная команда!!!" << std::endl;
+            counter_menu();
+        }
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    Counter operation;
+    counter_menu();
 
-    operation.counter_menu();
-
-
+    return 0;
 }
